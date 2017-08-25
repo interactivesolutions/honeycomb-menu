1c55e79135f41bf4e9f9c40fcb966ffc0fdfeffd
Mantas Paulauskas <mantopastas@gmail.com>
2017-08-25 15:54:28 +0300

    optimized menu getting

--- src/app/helpers/MenuHelper.php
+++ src/app/helpers/MenuHelper.php
@@ -192,7 +192,12 @@ class MenuHelper
 
         $items = $this->menuGroupsClass::select('id', 'name', 'sequence')
             ->with(['menu' => function ($query) use ($language) {
-                $query->where('language_code', $language)
+                $query->with([
+                    'children',
+                    'page' => function ($query) {
+                        $query->with('translations');
+                    },
+                ])->where('language_code', $language)
                     ->orderBy('sequence');
             }])
             ->where('language_code', $language)
